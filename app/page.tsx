e12b42a277b02f67848a9055094273b123c285b0
ayushisingh17 <ayushis1703@gmail.com>
2025-09-01 22:51:44 +0530

    new commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <Button>
+        Get Started
+      </Button>
+    </div>
+  );
+}
